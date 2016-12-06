283a660e4ad3a1854c71e1d5fdcf69e4bd6121e2
hanna15 <hanna15@ru.is>
2016-12-06 21:52:33 +0000

    try args

--- clean.py
+++ clean.py
@@ -1,2 +1,18 @@
-def test():
-	
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(
+	description="Clean download folder")
+
+parser.add_argument("downl_folder", metavar="FOLDER", type=str, help="the folder to be cleaned")
+parser.add_argument("dest_folder", metavar="FOLDER", type=str, help="the destination folder")
+
+def test(args.downl_folder, args.dest_folder):
+	downl = args.downl_folder
+	dest = args.dest_folder
+	print(downl)
+	print(dest)
+
+
+
+
