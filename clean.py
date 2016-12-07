c37baccc67721a3a525ff228b21bf4f8977733f4
Thorhildur <thorhildurt15@ru.is>
2016-12-07 22:15:04 +0000

    test with big bang theory

--- clean.py
+++ clean.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 from pathlib import Path
 import re
+import shutil
+
 parser = argparse.ArgumentParser(
 	description="Clean download folder")
 
@@ -12,13 +14,42 @@ args = parser.parse_args()
 downl = args.downl_folder
 dest = args.dest_folder
 p = Path().resolve() / downl
-result = list(p.glob("**/*.avi"))
-for i in result:
-	print()
-print(downl)
-print(dest)
-#print(p)
+result = list(p.glob("**/*"))
 
+new_dir = Path().resolve() / dest
+if not new_dir.exists():
+	new_dir.mkdir()
+	(new_dir / 'episodes').mkdir()
+	(new_dir / 'moives').mkdir()
+
+e = new_dir / 'episodes'
+m = new_dir / 'movies'
+
+#test with big bang theory
+for i in result:
+	if re.findall('(The.Big.Bang.Theory)(.*avi)', str(i)):
+		big_bang = e / 'The_Big_Bang_Theory'
+		if not big_bang.exists():
+			big_bang.mkdir()
+
+		if i.is_file():
+			shutil.copy(str(i), str(big_bang))
+
+
+
+
+
+
+
+#for i in result:
+#	print(i)
+
+	
+#print(p)
+#print(result)
+#print(down)
+#print(dest)
+#print
 #def test(downl, dest):
 
 #	print(downl)
