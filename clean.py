86d0e6849e81d492194d06dc29b01c166f81f20b
Thorhildur <thorhildurt15@ru.is>
2016-12-08 15:35:58 +0000

    Move episodes with right regexstring to correct folder in episodesfolder

--- clean.py
+++ clean.py
@@ -14,46 +14,57 @@ args = parser.parse_args()
 downl = args.downl_folder
 dest = args.dest_folder
 p = Path().resolve() / downl
-result = list(p.glob("**/*"))
+allFiles = list(p.glob("**/*")) #get all files
 
-new_dir = Path().resolve() / dest
+new_dir = Path().resolve() / dest 
 if not new_dir.exists():
 	new_dir.mkdir()
-	(new_dir / 'episodes').mkdir()
-	(new_dir / 'moives').mkdir()
+	(new_dir / 'episodes').mkdir() #create dir for all episodes
+	(new_dir / 'moives').mkdir() #create dir for all moies
 
-e = new_dir / 'episodes'
-m = new_dir / 'movies'
+episodePath = new_dir / 'episodes'
+moviePath = new_dir / 'movies'
 
-#test with big bang theory
-for i in result:
-	if re.findall('(The.Big.Bang.Theory)(.*avi)', str(i)):
-		big_bang = e / 'The_Big_Bang_Theory'
-		if not big_bang.exists():
-			big_bang.mkdir()
+regexepisodestring = '[sS][0-9]{2}[eE][0-9]{2}.*.avi' #all episodes that have number and .avi
 
-		if i.is_file():
-			shutil.copy(str(i), str(big_bang))
+episodeMatches = []
+for file in allFiles:
+	if re.findall(regexepisodestring, str(file)):
+		episodeMatches.append(file)
+
+untrackedPath = episodePath / 'UntrackedEpisodes'
+for match in episodeMatches:
+	if not untrackedPath.exists():
+		untrackedPath.mkdir()
+	shutil.copy(str(match), str(untrackedPath))
+
+allepisodes = list(untrackedPath.glob('**/*')) 
+splittedFilenames = []
+for episode in allepisodes:
+	splittedFilenames.append(str(episode).split('/')[-1])
+
+nameset = set() #set of foldernames for episodes
+for shortfilename in splittedFilenames:
+	newname = re.sub(regexepisodestring +'|\W','',shortfilename)
+	if newname:
+		nameset.add(newname.lower())
+
+for foldername in nameset: #create dir for common episodes
+	subepisodePath = episodePath / foldername.capitalize()
+	if not subepisodePath.exists():
+		subepisodePath.mkdir()
+
+for episode in allepisodes:
+	pathsToCheck = re.sub('\W','',str(episode)).lower() #clean path
+	for foldername in nameset:
+		if foldername in pathsToCheck: #check if the stringpath has the foldername in the path
+			#if the path contains the name, move the file to the correct folder with corresponding foldername
+			shutil.copy(str(episode), str(episodePath / foldername.capitalize()))
 
 
 
 
 
-
-
-#for i in result:
-#	print(i)
-
-	
-#print(p)
-#print(result)
-#print(down)
-#print(dest)
-#print
-#def test(downl, dest):
-
-#	print(downl)
-#	print(dest)
 
 
 
